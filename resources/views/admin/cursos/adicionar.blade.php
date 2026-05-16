83c63fc252089d6637a059b0a48d4e1970104417
Ana Clara de Menezes Coldibeli <ra2457016@MacBook-Air-2.local>
2026-05-16 11:59:12 -0300

    aula 2

--- resources/views/admin/cursos/adicionar.blade.php
+++ resources/views/admin/cursos/adicionar.blade.php
@@ -4,10 +4,10 @@
     <div class="container">
         <h3 class="center">Adicionar Curso</h3>
         <div class="row">
-            <form class="" action="{{route('admin.cursos.salvar')}}
-                    method = "post" enctype= "multipatt/form-data">
-                {{ csrf_field() }}
-                @include('admin.cursos.form')
+            <form class="" action="{{route('admin.cursos.salvar')}}"
+                    method="post" enctype="multipart/form-data">
+                @csrf
+                @include('admin.cursos._form')
                 <button class="btn deep-orange">Salvar</button>
             </form>
         </div>
--- resources/views/admin/cursos/index.blade.php
+++ resources/views/admin/cursos/index.blade.php
@@ -1,13 +1,15 @@
 @extends('layout.site')
-@section('titulo', 'Cursos')
+
+@section('titulo','Cursos')
+
 @section('conteudo')
 
 <div class='container'>
-<h3 class='center'>Lista de Cursos</h3>
+ <h3 class='center'>Lista de Cursos</h3>
     <div class='row'>
         <table>
             <thead>
-                <tr> <!-- CABECALHO -->
+                <tr>  <!-- CABECALHO -->
                     <td>Id</td><td>Titulo</td><td>Descrição</td>
                     <td>Publicado</td><td>Valor</td>
                     <td>Imagem</td>
@@ -15,22 +17,23 @@
                 </tr>
             </thead>
             <tbody>
-            @foreach($rows as $row) <!-- LOOP PRA LER A TABELA -->
+            @foreach($rows as $row)   <!-- LOOP PRA LER A TABELA -->
                 <tr>
                     <td>{{ $row->id }}</td><td>{{ $row->titulo }}</td><td>{{ $row->descricao }}</td>
                     <td>{{ $row->publicado }}</td><td>{{ $row->valor }}</td>
-                    <td><img src="{{ asset($row->imagem) }}" alt="{{ $row->titulo }}"></td>
-                    <td> <!-- COLUNA COM ALTERAR E EXCLUIR -->
+                    <td><img width="120" src="{{ asset($row->imagem) }}" alt="{{ $row->titulo }}"></td>
+                    <td>   <!-- COLUNA COM ALTERAR E EXCLUIR -->
                         <a class='btn deep-orange' href="{{ route('admin.cursos.editar',$row->id) }}">Alterar</a>
                         <a class='btn rede' href="{{ route('admin.cursos.excluir',$row->id) }}">Excluir</a>
                     </td>
                 </tr>
             @endforeach
             </tbody>
-        </table>
+        </table>   
     </div>
-    <div class='row'> <!-- BOTAO ADICIONAR -->
+    <div class='row'>  <!-- BOTAO ADICIONAR -->
         <a class='btn blue' href="{{ route('admin.cursos.adicionar')}}">Adicionar</a>
     </div>
 </div>
+
 @endsection
