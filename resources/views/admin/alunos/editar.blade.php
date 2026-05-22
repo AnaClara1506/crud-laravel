2ca78512f6cd0885c1a06085669759cd1ce74db0
Ana Clara de Menezes Coldibeli <ra2457016@MacBook-Air-2.local>
2026-05-22 09:58:21 -0300

    crediaria

---
+++ resources/views/admin/alunos/editar.blade.php
@@ -0,0 +1,18 @@
+@extends('layout.site')
+@section('titulo', 'Alunos')
+@section('conteudo')
+
+
+<div class="container">
+    <h3 class="center">Editando Aluno</h3>
+    <div class="row">
+        <form class="" action="{{route('admin.alunos.atualizar', $row->id)}}" 
+        method="post" enctype="multipart/form-data">
+        {{csrf_field()}}
+        <input type="hidden" name="_method" value="put">
+        @include('admin.alunos._form', ['cursos' => $cursos])
+        <button class="btn deep-orange">Salvar</button>
+        </form>
+    </div>
+</div>
+@endsection
