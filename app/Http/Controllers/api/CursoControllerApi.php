76c4b16a2515ab502cb6398e6598827ca0a2dfd4
Ana Clara de Menezes Coldibeli <ra2457016>
2026-08-08 12:14:00 -0300

    api

---
+++ app/Http/Controllers/api/CursoControllerApi.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Http\Controllers\api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Curso;
+use Illuminate\Http\Request;
+
+class CursoControllerApi extends Controller
+{
+    public function store(Request $req)
+    {
+        $novoCurso = Curso::create($req->all());
+        return response($novoCurso, 201);
+    }
+
+    public function index()
+    {
+        return Curso::all();
+    }
+
+    public function show($id)
+    {
+        $curso = Curso::find($id);
+
+        if ($curso != null) {
+            return response($curso, 200);
+        } else {
+            return response('', 404);
+        }
+    }
+
+    public function update(Request $req, $id)
+    {
+        $curso = Curso::find($id);
+
+        if ($curso != null) {
+            $curso->update($req->all());
+
+            return response('OK', 200);
+        } else {
+            return response('', 404);
+        }
+    }
+
+    public function destroy($id)
+    {
+        $cursoEncontrado = Curso::find($id);
+
+        if ($cursoEncontrado) {
+            if ($cursoEncontrado->delete()) {
+                return response('OK', 200);
+            } else {
+                return response('', 400);
+            }
+        } else {
+            return response('', 404);
+        }
+    }
+}
